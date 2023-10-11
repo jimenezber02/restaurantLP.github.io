d21f491071f9b99ddf16a77d216984648fa65e6f
jimenezber02 <88626108+jimenezber02@users.noreply.github.com>
2023-10-11 00:04:53 -0500

    update_style

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    // acción menú lateral
+    const aside_burguer = $('.btn-burguer');
+	if(aside_burguer){
+		const menu = $('.mobile');
+		aside_burguer.off('click').on('click',function(e){
+            e.preventDefault();
+    		$(menu).addClass('show-list-menu');
+		});
+
+        const burguer_close = $('#close-menu');
+        burguer_close.off('click').on('click',function(e){
+            $(menu).removeClass('show-list-menu');
+        });
+	}
+}
